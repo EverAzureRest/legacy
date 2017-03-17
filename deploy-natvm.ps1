705261fca67328d09f4dc6890325de699d4045c6
lorax79 <jordan.smith@powerhell.org>
2017-03-16 17:16:05 -0700

    removed proprietary info

--- deploy-natvm.ps1
+++ deploy-natvm.ps1
@@ -1,18 +1,18 @@
 $natVMParams = @{
-    'adminUsername' = "LMajors";
+    'adminUsername' = "";
     'vmNamePrefix' = "LHNATVM0";
     'virtualNetworkName' = "LH-INF-PD-VNET01";
     'virtualNetworkResourceGroup' = "LH-INF-PD-VNET01-RG";
     'subnetName' = "VdiSubnet";
-    'storageAccountName' = "lhinfpdnatstorage";
-    'publicIpAddressName' = "lhnatvm0-public";
-    'networkSecurityGroupName' = "lhNatNSG"
+    'storageAccountName' = "";
+    'publicIpAddressName' = "public";
+    'networkSecurityGroupName' = ""
 }
 
 Login-AzureRmAccount
 Get-AzureRmSubscription -SubscriptionName "Legacy IS Subscription" | Select-AzureRmSubscription
 
-$natVMRg = "LH-INF-PD-NAT-RG"
+$natVMRg = ""
 $natVMUri = "https://github.com/lorax79/legacy/raw/master/natVM.json"
 
 New-AzureRmResourceGroupDeployment -Name NATVM -ResourceGroupName $natVMRg -Mode Incremental -TemplateUri $natVMUri -TemplateParameterObject $natVMParams
